b7e152c22a16c30b5335b6aa7c8b72262e2d784e
zewexqa <domtui555@gmail.com>
2025-02-07 13:47:29 +0700

    lab17_1.cpp Finished

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -1,49 +1,66 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main(){
-	int nA,nB;
-	int *A,*B,*C;	
-	
-	cout << "Length of A: ";
-	cin >> nA;
-	
-	A = new int;
-	cout << "Input Array A: ";
-	for(int i=0; i < nA; i++) cin >> A[i];
-	
-	cout << "Length of B: ";
-	cin >> nB;
-	
-	B = new int;
-	cout << "Input Array B: ";
-	for(int i=0; i < nB; i++) cin >> B[i];
-	
-	C = new int;
-	for(int i=0; i < nA; i++) C[i] = new int;
-	
-	for(int i=0; i < nA; i++){
-		for(int j=0; j < nB; j++) {
-			C = A[i]*B[j];
-		}
-	}
-	
-	cout << "\n";
-	cout << setw(6) << " "; 
-	for(int i=0; i < nB; i++){
-		cout << setw(5) << B[i] << " ";
-	}
-	cout << "\n-----------------------------\n";
-	
-	for(int i=0; i < nA; i++){
-		cout << setw(5) << A[i] << " ";
-		for(int j=0; j < nB; j++) {
-			cout << setw(5) << C[i][j] << " ";
-		}
-		cout << "\n";
-	}
-     delete A,B,C; 
+int main() {
+    int nA, nB;
+    int *A, *B, **C;
 
-	return 0;
+
+    cout << "Length of A: ";
+    cin >> nA;
+    A = new int[nA];
+    
+    cout << "Input Array A: ";
+    for (int i = 0; i < nA; i++) {
+        cin >> A[i];
+    }
+
+
+    cout << "Length of B: ";
+    cin >> nB;
+    B = new int[nB];
+
+    cout << "Input Array B: ";
+    for (int i = 0; i < nB; i++) {
+        cin >> B[i];
+    }
+
+
+    C = new int*[nA]; 
+    for (int i = 0; i < nA; i++) {
+        C[i] = new int[nB];
+    }
+
+    for (int i = 0; i < nA; i++) {
+        for (int j = 0; j < nB; j++) {
+            C[i][j] = A[i] * B[j];
+        }
+    }
+
+
+    cout << "\n";
+    cout << setw(6) << " ";
+    for (int i = 0; i < nB; i++) {
+        cout << setw(5) << B[i] << " ";
+    }
+    cout << "\n-----------------------------\n";
+
+    for (int i = 0; i < nA; i++) {
+        cout << setw(5) << A[i] << " ";
+        for (int j = 0; j < nB; j++) {
+            cout << setw(5) << C[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+
+    delete[] A;
+    delete[] B;
+    for (int i = 0; i < nA; i++) {
+        delete[] C[i];
+    }
+    delete[] C;
+
+    return 0;
 }
